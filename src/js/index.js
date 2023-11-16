542fa62e15171b9cdc33a65eabc488b4436f79c0
Proxx07 <zakir.pulatov97@gmail.com>
2023-11-16 18:49:52 +0500

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/* Main entrypoint js */
+import localization from './localization';
+
+localization();
+
+console.log('ENV', process.env.NODE_ENV);
---
+++ src/js/localization.js
@@ -0,0 +1,44 @@
+import languages from './languages';
+
+const localization = () => {
+  const language = document.querySelector('.language');
+  if (!language) return;
+
+  const currentLang = language.querySelector('.lang-title');
+  const languageButtons = language.querySelectorAll('.lang-buttons button');
+
+  const changeLanguages = () => {
+    const ln = localStorage.getItem('lang') || 'ru';
+    const languageKeys = !languages[ln] ? languages.ru : languages[ln];
+
+    Object.keys(languageKeys).forEach((key) => {
+      const item = document.querySelector(`[data-ln=${key}]`);
+      item.innerHTML = languageKeys[key];
+    });
+
+    currentLang.textContent = [...languageButtons].filter((btn) => btn.dataset.key === ln)[0].textContent; //eslint-disable-line
+  };
+
+  document.addEventListener('click', (event) => {
+    if (event.target.closest('.language')) return;
+    language.classList.remove('opened');
+  });
+
+  currentLang.addEventListener('click', () => {
+    language.classList.toggle('opened');
+  });
+
+  languageButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      localStorage.setItem('lang', button.dataset.key);
+      changeLanguages();
+      language.classList.remove('opened');
+    });
+  });
+
+  if (localStorage.getItem('lang') !== 'ru') {
+    changeLanguages();
+  }
+};
+
+export default localization;
