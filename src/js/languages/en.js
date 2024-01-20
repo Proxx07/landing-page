63c557deb990835632c1e80c7ea80fe2f60952dc
Proxx07 <zakir.pulatov97@gmail.com>
2024-01-20 15:45:40 +0500

    locales reworked

--- src/js/languages/en.js
+++ src/js/languages/en.js
@@ -1,69 +1,69 @@
 export default {
   // menu items
   menu_main: 'Home',
-  menu_connect: 'Contact',
+  menu_connect: 'Contact us',
   menu_about: 'About the product',
   menu_advantages: 'Advantages',
   menu_business: 'For business',
-  menu_customer: 'For the customer',
-  menu_script: 'Use cases',
+  menu_customer: 'For clients',
+  menu_script: 'Usage scenarios',
 
   // text blocks
-  text_block_title1: 'Modern solution for biometric identification',
-  text_block_text1: 'Camera of a mobile device or personal computer',
-  text_block_title2: 'Identification and verification with devices of any type',
+  text_block_title1: 'Up-to-date solution for biometric identification',
+  text_block_text1: 'Camera on a mobile device or PC',
+  text_block_title2: 'Identification and verification using all types of devices',
   text_block_text2: `
-  <p> PC, tablet, smartphone, in the web browser and in mobile applications </p>
-  <p> Provides burglary resistance and Vitality check </p>
+  <p> Browser or mobile app identification on a PC, tablet or smartphone. </p>
+  <p> Detects passive facial liveness and provides real-time protection via Liveness technology. </p>
   <p> Reduces the risk of fraud </p>`,
 
   text_block_title3: 'Biometric verification',
   text_block_text3: `
     <p>The fastest and most reliable way to verify a user's identity and protect your business from fraud.</p>
-    <p>With the solution, you can be sure that the client is who he says he is and that the claimed identity is associated with a real person.</p>`,
+    <p>Using this solution, you can be sure in user's identity and that the claimed identity is linked to a real person.</p>`,
 
   // capabilities
-  capability_1: 'Face verification with a computer or mobile device camera',
-  capability_2: 'User identification by PINFL',
-  capability_3: 'Detection of glare, shadows, and face position for human identification',
-  capability_4: 'Analytics and statistics on biometric data and devices',
-  capability_5: 'Easy integration via AbleID API',
-  capability_6: 'Conducts checks against government databases',
+  capability_1: 'Face verification using a computer or mobile device camera',
+  capability_2: 'User identification using ID number',
+  capability_3: 'Determination of highlights, shadows, facial position for person identification',
+  capability_4: 'Analytics and statistics of biometric and devices data',
+  capability_5: 'Easy integration via API',
+  capability_6: 'Verify identity with government IDs',
 
   // for business
   for_business_title: 'For business',
-  for_business_subtitle: 'Identification from a computer and mobile device',
-  for_business_form_title: 'Face Identification',
-  for_business_form_text: 'Center the face',
+  for_business_subtitle: 'Identification using a computer and mobile device',
+  for_business_form_title: 'Face identification',
+  for_business_form_text: 'Center your face',
   for_business_form_button: 'Start',
-  for_business_item1_title: 'Unparalleled',
-  for_business_item1_text: 'Identification on PC and mobile device. In the web browser and in the application',
-  for_business_item2_title: 'Security',
-  for_business_item2_text: 'Strong encryption of sensitive data',
+  for_business_item1_title: 'Has no analogues',
+  for_business_item1_text: 'Identification using PC and mobile device. In web browser and app',
+  for_business_item2_title: 'Safety',
+  for_business_item2_text: 'Secure encryption of sensitive data',
   for_business_item3_title: 'Speed',
-  for_business_item3_text: 'Face matching and identity verification completed in seconds',
-  for_business_item4_title: 'Resiliency',
-  for_business_item4_text: 'High SLA ensuring double reservation of computing power',
+  for_business_item3_text: 'Face matching and authentication are completed in seconds',
+  for_business_item4_title: 'Fault tolerance',
+  for_business_item4_text: 'High SLA providing double reservation of computing power',
   for_business_item5_title: 'Support',
-  for_business_item5_text: 'Resolution of any technical issues as soon as possible',
+  for_business_item5_text: 'Solving any technical issues in the shortest time possible',
   for_business_item6_title: 'Availability',
-  for_business_item6_text: 'No costs for equipment, staff training, support and maintenance',
+  for_business_item6_text: 'No expenses for equipment, staff training, support and maintenance',
 
   // for_clients
-  for_clients_title: 'For the client',
+  for_clients_title: 'For clients',
   for_clients_item1_title: 'Fast',
-  for_clients_item1_text: 'It is enough to scan your face with the camera of your computer or mobile device',
-  for_clients_item2_title: 'Convenient',
-  for_clients_item2_text: 'No need to visit other authorities and collect additional documents',
+  for_clients_item1_text: 'All you need to do is scan the face with the camera of your PC or mobile device.',
+  for_clients_item2_title: 'Comfortable',
+  for_clients_item2_text: 'There is no need to visit other authorities and provide additional documents',
   for_clients_item3_title: 'No bureaucracy',
-  for_clients_item3_text: 'No certificates are needed to confirm your identity',
-  for_clients_item4_title: 'Confidential',
-  for_clients_item4_text: 'Information is not shared with third parties',
+  for_clients_item3_text: 'No other documents required for identity confirmation',
+  for_clients_item4_title: 'Confidentially',
+  for_clients_item4_text: 'Information is not transferred to third parties',
 
   // use_cases
-  use_case_title: 'Use cases',
-  use_case_item1_title: 'Mobile operator',
-  use_case_item1_text: 'Remote identification when registering a contract for communication services',
+  use_case_title: 'Usage scenarios',
+  use_case_item1_title: 'Mobile provider',
+  use_case_item1_text: 'Remote identification when signing contract for mobile services',
   use_case_item2_title: 'Banks and fintech products',
   use_case_item2_text: 'Remote identification when drawing up an agreement for services and banking services',
   use_case_item3_title: 'Government services',
@@ -72,11 +72,11 @@ export default {
   // callback_form
   callback_form_title: 'Feedback form',
   callback_form_tel: 'Phone number',
-  callback_form_email: 'E-mail',
+  callback_form_email: 'Email',
   callback_form_comment: 'Your message',
   callback_form_button: 'Send',
   callback_form_footer_title: 'Contacts',
 
   contact_us: 'Contact us',
-  developed_in: 'Developed in',
+  developed_in: 'Designed in',
 };
